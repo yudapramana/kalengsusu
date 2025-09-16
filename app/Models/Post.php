80cb67210ecafb647f09815a2937de200f7ce441
pramana yuda <pramanayuda772@gmail.com>
2025-09-16 15:28:35 +0700

    fix landing

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -141,7 +141,7 @@ class Post extends Model
 
     public function getRectangleCoverImageAttribute()
     {
-        $placeholder = "http://res.cloudinary.com/dezj1x6xp/image/upload/v1698216019/PandanViewMandeh/video-placeholder_kfnvxm.jpg";
+        $placeholder = "http://res.cloudinary.com/dezj1x6xp/image/upload/c_fill,ar_16:9,q_50/v1698216019/PandanViewMandeh/video-placeholder_kfnvxm.jpg";
         $separator = '/upload/';
 
         $cover = $this->attributes['cover'] ?? null;
