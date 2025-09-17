265370047d639783e9ffafedde166e1db0bd2b47
pramana yuda <pramanayuda772@gmail.com>
2025-09-17 19:54:27 +0700

    perbaiki footer

--- resources/views/layouts/landing/v2/footer.blade.php
+++ resources/views/layouts/landing/v2/footer.blade.php
@@ -24,12 +24,7 @@
 
                     </ul>
 
-                    <h6 class="widget_title mt-3 mb-1">STATISTIK KUNJUNGAN</h6>
-                    <ul class="list_none widget_links">
-                        <li><a>Hari ini: {{ $daily }}</a></li>
-                        <li><a>Bulan ini: {{ $monthly }}</a></li>
-                        <li><a>Tahun ini: {{ $yearly }}</a></li>
-                    </ul>
+
 
 
 
@@ -37,18 +32,25 @@
 
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
-                    <h6 class="widget_title">LINK EKSTRA</h6>
+                    <h6 class="widget_title mb-1">LINK EKSTRA</h6>
                     <ul class="list_none widget_links">
-                        <li><a href="https://sumbar.kemenag.go.id/">Portal Kemenag Provinsi</a></li>
-                        <li><a href="https://kemenag.go.id/">Kemenag RI</a></li>
-                        <li><a href="https://ppid.kemenag.go.id/">PPID Kemenag RI</a></li>
-                        <li><a href="https://pusaka.kemenag.go.id/">Pusaka Kemenag RI</a></li>
-                        <li><a href="{{ config('isec.base_url') }}/controlcenter/login">Login</a></li>
+                        <li style="padding: 0; !important"><a href="https://sumbar.kemenag.go.id/">Portal Kemenag Provinsi</a></li>
+                        <li style="padding: 0; !important"><a href="https://kemenag.go.id/">Kemenag RI</a></li>
+                        <li style="padding: 0; !important"><a href="https://ppid.kemenag.go.id/">PPID Kemenag RI</a></li>
+                        <li style="padding: 0; !important"><a href="https://pusaka.kemenag.go.id/">Pusaka Kemenag RI</a></li>
+                        <li style="padding: 0; !important"><a href="{{ config('isec.base_url') }}/controlcenter/login">Login</a></li>
+                    </ul>
+
+                    <h6 class="widget_title mt-3 mb-1">STATISTIK KUNJUNGAN</h6>
+                    <ul class="list_none widget_links">
+                        <li style="padding: 0; !important"><a>Hari ini: {{ $daily }}</a></li>
+                        <li style="padding: 0; !important"><a>Bulan ini: {{ $monthly }}</a></li>
+                        <li style="padding: 0; !important"><a>Tahun ini: {{ $yearly }}</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-6 col-md-6 mb-4 mb-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
                     <h6 class="widget_title">Peta Kantor</h6>
-                    <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510629.2213989474!2d99.7828554734375!3d-0.9317269999999771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b92894aa00af%3A0x3453626808b42408!2sKanwil%20Kementerian%20Agama%20Provinsi%20Sumatera%20Barat!5e0!3m2!1sen!2sid!4v1694596922042!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe style="border:0; width: 100%; height: 250px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510629.2213989474!2d99.7828554734375!3d-0.9317269999999771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b92894aa00af%3A0x3453626808b42408!2sKanwil%20Kementerian%20Agama%20Provinsi%20Sumatera%20Barat!5e0!3m2!1sen!2sid!4v1694596922042!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
